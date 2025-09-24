114113657cf0098095271671fa15f0fa0cb0df32
robbysuganda <robbyxsuganda@gmail.com>
2025-09-24 13:58:21 +0700

    chore: add vercel.json

--- fe-sjapoutbound/next.config.ts
+++ fe-sjapoutbound/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "standalone", // untuk deploy di Vercel, boleh ada
   images: {
     remotePatterns: [
       {
